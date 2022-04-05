6244150923856317e548b17b473bb13fbd0e9693
dil-zmolnar <zmolnar@diligent.com>
2022-04-05 13:41:13 +0200

    Add tests

---
+++ Collect-PackagesToDelete.ps1
@@ -0,0 +1,38 @@
+function Collect-PackagesToDelete {
+    param (
+        [Parameter(Mandatory)]
+        [object[]] $packageList,
+
+        [Parameter(Mandatory)]
+        [int] $retentionCount,
+
+        [Parameter(Mandatory)]
+        [int] $retentionAge,
+
+        [Parameter(Mandatory)]
+        [int] $retentionHardLimit
+    )
+
+    $packagesToDelete = @()
+
+    $packageRetentionDate = (Get-Date).AddDays(-$retentionAge)
+    $retainedPackageCount = 0
+
+    $packageList = $packageList | sort uploaded_at -Descending
+   
+    foreach ($package in $packageList) {
+        if ($retainedPackageCount -ge $retentionCount) {
+            if ($package.uploaded_at -lt $packageRetentionDate) {
+                $packagesToDelete += $package
+            } elseif ($retainedPackageCount -ge $retentionHardLimit) {
+                $packagesToDelete += $package
+            } else {
+                $retainedPackageCount++
+            }
+        } else {
+            $retainedPackageCount++
+        }
+    }
+
+    return ,$packagesToDelete
+}
