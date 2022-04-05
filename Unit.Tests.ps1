7a637bb4b7e4f72d339a7e95d05e64179c858701
dil-zmolnar <zmolnar@diligent.com>
2022-04-05 14:19:21 +0200

    SPE-1231 Try to test imported function

--- Unit.Tests.ps1
+++ Unit.Tests.ps1
@@ -7,4 +7,23 @@ Describe "Unit tests" {
     It "Test whether README exists" {
         Test-Path README.md | Should -Be $true
     }
+
+    It "Should delete the older packages which are younger than the retention age but over the hard limit" {
+
+        # Arrange
+        $oldestPackageUploadTime = (Get-Date).AddDays(-5)
+        $packageList = @()
+        $packageList += @{ name = "package1"; format = "helm"; uploaded_at = $oldestPackageUploadTime; tags = @{ info = @("released") } }
+        $packageList += @{ name = "package1"; format = "helm"; uploaded_at = (Get-Date).AddDays(-4); tags = @{ info = @("released") } }
+        $packageList += @{ name = "package1"; format = "helm"; uploaded_at = (Get-Date).AddDays(-3); tags = @{ info = @("released") } }
+        $packageList += @{ name = "package1"; format = "helm"; uploaded_at = (Get-Date).AddDays(-2); tags = @{ info = @("released") } }
+        $packageList += @{ name = "package1"; format = "helm"; uploaded_at = (Get-Date).AddDays(-1); tags = @{ info = @("released") } }
+
+        # Act
+        $result = Find-PackagesToDelete -packageList $packageList -retentionCount 2 -retentionAge 14 -retentionHardLimit 4
+
+        # Assert
+        $result | Should -HaveCount 1
+        $result[0].uploaded_at | Should -Be $oldestPackageUploadTime
+    }
 }
